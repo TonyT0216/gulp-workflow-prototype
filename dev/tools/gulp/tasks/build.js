f9271cd61eb98728b1d9ccca0d1ea75c6b68a2cd
Tony Toscano <tonymichael16@gmail.com>
2020-10-29 19:29:39 -0400

    Cut out unneeded code.

--- dev/tools/gulp/tasks/build.js
+++ dev/tools/gulp/tasks/build.js
@@ -1,11 +1,17 @@
 'use strict';
 
+const concat = require('gulp-concat');
+const flatmap = require('gulp-flatmap');
+const {dest, src} = require('gulp');
+
 const appPaths = require('../app-configs/appPaths');
 const appSettings = require('../app-configs/appSettings');
+const themeSet = require('../configs/themes');
+
 
 function buildJSFiles(done) {
     if (appSettings.scripts === false) {
         return done();
     }
 
-}
+};
