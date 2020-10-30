54aab7ca27ee084b5721f74bc1d0f08a4181a33f
ttoscano <atoscano@it18.holts.com>
2020-10-30 16:55:51 -0400

    More precise pathing for vital directories.

--- dev/tools/gulp/app-configs/appPaths.js
+++ dev/tools/gulp/app-configs/appPaths.js
@@ -1,9 +1,19 @@
 'use strict';
 
 module.exports = {
-    rootAdminPanelThemeGlob: 'app/design/adminhtml/**',
-    rootCustomModulesGlob: 'app/code/**',
-    rootStaticFolder: 'pub/static/',
-    rootStaticFolderGlob: 'pub/static/**',
-    rootStorefrontThemeGlob: 'app/design/frontend/**'
+    jsFiles: {
+        moduleInputDir: 'app/code/**/**/view/**/web/js/*.js',
+        themeInputDir: 'app/design/**/**/**/web/js/*/*.js',
+        outputDir: 'pub/static/js/*'
+    },
+    lessFiles: {
+        moduleInputDir: 'app/code/**/**/view/**/web/css/source/*.less',
+        themeInputDir: 'app/design/**/**/**/**/web/css/source/*.less',
+        outputDir: 'pub/static/css/*'
+    },
+    // rootAdminPanelThemeGlob: 'app/design/adminhtml/**',
+    // rootCustomModulesGlob: 'app/code/**',
+    // rootStaticFolder: 'pub/static/',
+    // rootStaticFolderGlob: 'pub/static/**',
+    // rootStorefrontThemeGlob: 'app/design/frontend/**'
 };
